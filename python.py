17fb86630445976f0b2a4b1e93f78e993c2ab8cf
YakoSec <93792838+YakoSec@users.noreply.github.com>
2023-02-07 23:10:51 +0330

    Create python.py

---
+++ python.py
@@ -0,0 +1,11 @@
+# Mr. Yako
+# Instagram: @YakoSec
+# Fake information generator.
+
+from faker import Faker
+fake = Faker()
+
+print(fake.name())
+print(fake.email())
+print(fake.country())
+print(fake.profile())                                                        
